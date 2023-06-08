a4e9d30c808cce06fb8d071848fccdce7bf691a9
al1yew <vasifja@code.edu.az>
2023-06-08 22:12:34 +0400

    commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,9 +2,9 @@ const AboutSection = () => {
     return (
         <section className="about" id="About">
             <div className="container">
-                <div className="all">
+                <div className="row all">
                     <h1>About Me</h1>
-                    <div className="element order-1 order-md-1 col-lg-5-8 col-md-12 col-12">
+                    <div className="element order-1 order-lg-1 order-sm-1 order-md-1 col-lg-5-8 col-md-12 col-12">
                         <div className="col-lg-5-5 col-md-5-5 col-5-5">
                             <svg
                                 viewBox="0 0 287 247"
@@ -61,7 +61,7 @@ const AboutSection = () => {
                             <p>Creativity</p>
                         </div>
                     </div>
-                    <div className="textelement order-3 order-md-2 col-lg-5-8 col-12 col-md-12">
+                    <div className="textelement order-3 order-sm-2 order-md-2 order-lg-2 col-lg-5-8 col-12 col-md-12">
                         As a skilled software developer, I excel in crafting
                         immersive digital experiences that captivate users. With
                         a strong focus on aesthetic design and cutting-edge
@@ -70,7 +70,7 @@ const AboutSection = () => {
                         stay ahead of industry trends and deliver innovative
                         solutions aligned with evolving client needs.
                     </div>
-                    <div className="textelement order-4 order-md-3 col-lg-5-8 col-12 col-md-12">
+                    <div className="textelement order-4 order-sm-3 order-md-3 order-lg-3 col-lg-5-8 col-12 col-md-12">
                         Effective time management is at the core of my workflow,
                         ensuring seamless project execution and meeting
                         deadlines without compromising quality. I possess
@@ -78,7 +78,7 @@ const AboutSection = () => {
                         complex coding challenges and developing robust,
                         scalable web applications.
                     </div>
-                    <div className="element order-2 order-md-4 col-lg-5-8 col-md-12 col-12">
+                    <div className="element order-2 order-lg-4 order-sm-4 order-md-4 col-lg-5-8 col-md-12 col-12">
                         <div className="special col-lg-5-5 col-md-5-5 col-5-5">
                             <svg
                                 fill="none"
