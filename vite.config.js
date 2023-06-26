dc40dca529aac1598cf3745ffd41f519c9cd1853
al1yew <vasifja@code.edu.az>
2023-06-26 15:51:47 +0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        sitemap({
-            base: "https://vasifaliyev.com",
-            exclude: ["*", "/404"],
-        }),
-    ],
+    plugins: [react()],
     server: {
         host: "0.0.0.0",
     },
