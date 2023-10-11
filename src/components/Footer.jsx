0ae733cebf279908a65c5ec8c0a568f3032285fb
al1yew <vasifaliyev.baku@gmail.com>
2023-10-11 12:04:57 +0200

    changed links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,67 +5,64 @@ import CV from "../assets/files/CV.pdf";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 const Footer = () => {
-    const { isDarkTheme } = useThemeContext();
+  const { isDarkTheme } = useThemeContext();
 
-    const downloadFile = () => {
-        const link = document.createElement("a");
-        link.href = CV;
-        link.download = "VASIF_ALIYEV_CV.pdf";
-        link.target = "_blank";
+  const downloadFile = () => {
+    const link = document.createElement("a");
+    link.href = CV;
+    link.download = "VASIF_ALIYEV_CV.pdf";
+    link.target = "_blank";
 
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-    return (
-        <section className="footer">
-            <div className="container">
-                <div className="all row">
-                    <div className="col-lg-2 col-12 col-md-3 col-sm-4 first">
-                        <img
-                            src={isDarkTheme ? imagewhite : imageblack}
-                            alt="vasif aliyev"
-                        />
-                    </div>
-                    <div className="col-lg-6 col-5 col-md-3 col-sm-4 second">
-                        <span>
-                            <a
-                                href="https://github.com/al1yew"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FaGithub />
-                            </a>
-                        </span>
-                        <span>
-                            <a
-                                href="https://www.linkedin.com/in/vasif-aliyev/"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FaLinkedinIn />
-                            </a>
-                        </span>
-                        <span>
-                            <a
-                                href="mailto:vasifaliyev.baku@gmail.com"
-                                target="_blank"
-                            >
-                                <FiMail />
-                            </a>
-                        </span>
-                    </div>
-                    <button
-                        type="button"
-                        className="col-lg-2 col-5 col-md-3 col-sm-4"
-                        onClick={downloadFile}
-                    >
-                        Download My CV
-                    </button>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="footer">
+      <div className="container">
+        <div className="all row">
+          <div className="col-lg-2 col-12 col-md-3 col-sm-4 first">
+            <img
+              src={isDarkTheme ? imagewhite : imageblack}
+              alt="vasif aliyev"
+            />
+          </div>
+          <div className="col-lg-6 col-5 col-md-3 col-sm-4 second">
+            <span>
+              <a
+                href="https://github.com/al1yew"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </span>
+            <span>
+              <a
+                href="https://www.linkedin.com/in/aliyev-vasif/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </span>
+            <span>
+              <a href="mailto:vasifaliyev.work@gmail.com" target="_blank">
+                <FiMail />
+              </a>
+            </span>
+          </div>
+          <button
+            type="button"
+            className="col-lg-2 col-5 col-md-3 col-sm-4"
+            onClick={downloadFile}
+          >
+            Download My CV
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Footer;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,84 +7,81 @@ import img3 from "../assets/images/1-04.png";
 import img4 from "../assets/images/1-05.png";
 
 const HeroSection = () => {
-    const downloadFile = () => {
-        const link = document.createElement("a");
-        link.href = CV;
-        link.download = "VASIF_ALIYEV_CV.pdf";
-        link.target = "_blank";
+  const downloadFile = () => {
+    const link = document.createElement("a");
+    link.href = CV;
+    link.download = "VASIF_ALIYEV_CV.pdf";
+    link.target = "_blank";
 
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-    return (
-        <section className="home" id="Home">
-            <div className=" all">
-                <div className="col-lg-6 col-md-6 col-sm-12 col-12 left">
-                    <p>Hello, My name is</p>
-                    <p>Vasif Aliyev</p>
-                    <p>and I am</p>
-                    <p>Software Developer</p>
-                    <p>
-                        Welcome to my digital realm, where creativity meets
-                        functionality. Join me on this exciting journey as we
-                        build remarkable software solutions together.
-                    </p>
-                    <div className="bottom col-lg-12 col-12 col-md-12">
-                        <button
-                            type="button"
-                            className="col-lg-5-5 col-5-5 col-md-5-5"
-                            onClick={downloadFile}
-                        >
-                            Download My CV
-                        </button>
-                        <div className="icons col-lg-5-5 col-5-5 col-md-5-5">
-                            <span>
-                                <a
-                                    href="https://github.com/al1yew"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <FaGithub />
-                                </a>
-                            </span>
-                            <span>
-                                <a
-                                    href="https://www.linkedin.com/in/vasif-aliyev/"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <FaLinkedinIn />
-                                </a>
-                            </span>
-                            <span>
-                                <a
-                                    href="mailto:vasifaliyev.baku@gmail.com"
-                                    target="_blank"
-                                >
-                                    <FiMail />
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-12 col-12 right">
-                    <div className="imgkeeper">
-                        <img src={img1} alt="developer" />
-                    </div>
-                    <div className="imgkeeper">
-                        <img src={img2} alt="code" />
-                    </div>
-                    <div className="imgkeeper">
-                        <img src={img3} alt="software" />
-                    </div>
-                    <div className="imgkeeper">
-                        <img src={img4} alt="website" />
-                    </div>
-                </div>
+  return (
+    <section className="home" id="Home">
+      <div className=" all">
+        <div className="col-lg-6 col-md-6 col-sm-12 col-12 left">
+          <p>Hello, My name is</p>
+          <p>Vasif Aliyev</p>
+          <p>and I am</p>
+          <p>Software Developer</p>
+          <p>
+            Welcome to my digital realm, where creativity meets functionality.
+            Join me on this exciting journey as we build remarkable software
+            solutions together.
+          </p>
+          <div className="bottom col-lg-12 col-12 col-md-12">
+            <button
+              type="button"
+              className="col-lg-5-5 col-5-5 col-md-5-5"
+              onClick={downloadFile}
+            >
+              Download My CV
+            </button>
+            <div className="icons col-lg-5-5 col-5-5 col-md-5-5">
+              <span>
+                <a
+                  href="https://github.com/al1yew"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://www.linkedin.com/in/aliyev-vasif/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </span>
+              <span>
+                <a href="mailto:vasifaliyev.work@gmail.com" target="_blank">
+                  <FiMail />
+                </a>
+              </span>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-12 col-12 right">
+          <div className="imgkeeper">
+            <img src={img1} alt="developer" />
+          </div>
+          <div className="imgkeeper">
+            <img src={img2} alt="code" />
+          </div>
+          <div className="imgkeeper">
+            <img src={img3} alt="software" />
+          </div>
+          <div className="imgkeeper">
+            <img src={img4} alt="website" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default HeroSection;
